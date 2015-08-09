d8c63f8c4b572ec38883d03e5bb7373412c56fae
Ayman <ahmed.a.sirajuddin@gmail.com>
2015-08-09 01:29:07 -0400

    modified InsertionSort.java

--- sorts/InsertionSort.java
+++ sorts/InsertionSort.java
@@ -1,26 +1,11 @@
 import java.util.Arrays;
 
 public class InsertionSort {
-    public static void main (String[] args) { // main method must be given an array of strings called args
+    public static void main (String[] args) {
         int hand[] = new int[] {1, 5, 2, 7, 3, -10, -10, 9, 8, 10, 15, 0, 12, 5, 6, 9, 18, 4, 2};
         sort(hand);
     }
     
-    /*
-    public static void sort (int[] hand) {
-        for (int index = 1; index < hand.length; index++) {
-            int key = hand[index];
-            int prev = index - 1;
-            while (prev >= 0 && hand[prev] > key) {
-                hand[prev + 1] = hand[prev];
-                prev = prev - 1;
-            }
-            hand[prev + 1] = key;
-        }
-        System.out.println(Arrays.toString(hand));
-    }
-    */
-
     public static void sort (int[] hand) {
         for (int index = 1; index < hand.length; index++) {
             int key = hand[index];
